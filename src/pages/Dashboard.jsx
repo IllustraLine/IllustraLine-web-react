79e32d0fbcf8360b188dded51f5d9a5139edc09a
Ijon6k <erikghafari@gmail.com>
2024-06-28 23:11:42 +0700

    added style

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,41 @@
 import Navbar from "../components/layout/Navbar";
 import Page from "../components/layout/Page";
+import Tabs from "../components/layout/Tabs";
 
 const Dashboard = () => {
+  const user = {
+    name: "Erikghafari",
+    wallet: 1000000,
+  };
+  const categories = ["On-going", "Finished"];
+
+  const handleTabChange = (category) => {
+    setSelectedCategory(category);
+  };
+
   return (
     <Page>
       <Navbar />
-      <section className="mx-auto min-h-screen w-screen max-w-screen-xl border border-red-500">
-        <h1>Dashboard</h1>
+      <section className="mx-auto min-h-screen w-screen max-w-screen-xl rounded-xl border border-red-500 p-5">
+        <div className="flex h-60 w-full items-center justify-between rounded-xl bg-blue-100 px-20">
+          <span>
+            <p className="text-xl font-bold">Hai ,</p>
+            <h1 className="text-3xl font-black">{user.name}</h1>
+          </span>
+
+          <div>
+            <div className="w-fit border-b-2 border-black">
+              wallet
+              <p className="text-xl font-black">
+                {user.wallet.toLocaleString("id-ID")}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <main className="mt-5 min-h-screen w-full bg-red-50">
+          <Tabs categories={categories} onTabChange={handleTabChange}></Tabs>
+        </main>
       </section>
     </Page>
   );
